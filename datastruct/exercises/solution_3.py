08c923a19e0189cbcfaa2b74d8412d5ccf074aaf
Jhonkyyy <jhonfre0212@gmail.com>
2025-03-05 11:35:27 -0500

    exercise 3 solution

--- datastruct/exercises/solution_3.py
+++ datastruct/exercises/solution_3.py
@@ -2,13 +2,27 @@ from datastruct.classes.lists import LinkedList
 
 
 def sort_list_by_insertion(linked_list: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
-    head =  linked_list.head
+    if linked_list.is_empty() or linked_list.head.next is None:
+        return linked_list
 
-    temp = head.next
+    sorted_list = LinkedList[int]()
+    current = linked_list.head
 
-    while head.next is not None:
-        if temp < head:
-            head = temp
+    while current:
+        next_node = current.next
 
-    pass
+        if sorted_list.is_empty() or sorted_list.head.data >= current.data:
+            current.next = sorted_list.head
+            sorted_list.head = current
+
+        else:
+            temp = sorted_list.head
+            while temp.next and temp.next.data < current.data:
+                temp = temp.next
+            current.next = temp.next
+            temp.next = current
+
+        current = next_node
+
+    return sorted_list
+
