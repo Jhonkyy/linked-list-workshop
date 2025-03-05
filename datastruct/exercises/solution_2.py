533d9e373f772aa5ca5562d40c86c287373c19b3
Jhonkyyy <jhonfre0212@gmail.com>
2025-03-05 11:10:31 -0500

    exercise 2 solution

--- datastruct/exercises/solution_2.py
+++ datastruct/exercises/solution_2.py
@@ -1,9 +1,26 @@
 from datastruct.classes.lists import LinkedList
+from datastruct.classes.lists import Node
 
 
 def swap_nodes_in_pairs(linked_list: LinkedList[int]) -> LinkedList[int]:
-    head = linked_list.head
-    ntswap = head.next.next
-    head = ntswap
+    if linked_list.is_empty() or linked_list.head.next is None:
+        return linked_list
 
-    pass
+    temp = Node(0)
+    temp.next = linked_list.head
+    prev = temp
+    current = linked_list.head
+
+    while current and current.next:
+        next_pair = current.next.next
+        second = current.next
+
+        second.next = current
+        current.next = next_pair
+        prev.next = second
+
+        prev = current
+        current = next_pair
+
+    linked_list.head = temp.next
+    return linked_list
